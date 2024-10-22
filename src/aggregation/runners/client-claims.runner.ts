f2cf4c7f25038e7f58c6c9f2a4ff0da7113c0aba
Mateusz Mazur <mateusz.mazur@neti-soft.com>
2024-10-22 12:41:45 +0200

    ClientClaimsRunner

--- src/aggregation/runners/client-claims.runner.ts
+++ src/aggregation/runners/client-claims.runner.ts
@@ -3,24 +3,64 @@ import { PrismaDmobService } from 'src/db/prismaDmob.service';
 import { FilSparkService } from 'src/filspark/filspark.service';
 import { AggregationRunner } from '../aggregation-runner';
 import { AggregationTable } from '../aggregation-table';
-import { getClientClaimsHourly } from '../../../prisma/generated/client/sql';
+import { PostgresService } from '../../db/postgres.service';
+import { QueryIterablePool } from 'pg-iterator';
 
 export class ClientClaimsRunner implements AggregationRunner {
   async run(
     prismaService: PrismaService,
     _prismaDmobService: PrismaDmobService,
     _filSparkService: FilSparkService,
+    postgresService: PostgresService,
   ): Promise<void> {
-    const result = await prismaService.$queryRawTyped(getClientClaimsHourly());
+    prismaService.$transaction(async (tx) => {
+      const queryIterablePool = new QueryIterablePool<{
+        client: string;
+        hour: Date;
+        total_deal_size: bigint | null;
+      }>(postgresService.pool);
 
-    const data = result.map((row) => ({
-      hour: row.hour,
-      client: row.client,
-      total_deal_size: row.total_deal_size,
-    }));
+      const i = queryIterablePool.query(`select client,
+                                                hour,
+                                                sum(total_deal_size)::bigint as total_deal_size
+                                         from unified_verified_deal_hourly
+                                         group by client,
+                                                  hour;`);
 
-    await prismaService.$executeRaw`truncate client_claims_hourly;`;
-    await prismaService.client_claims_hourly.createMany({ data });
+      const data: {
+        client: string;
+        hour: Date;
+        total_deal_size: bigint | null;
+      }[] = [];
+
+      let isFirstInsert = true;
+      for await (const rowResult of i) {
+        data.push({
+          hour: rowResult.hour,
+          client: rowResult.client,
+          total_deal_size: rowResult.total_deal_size,
+        });
+
+        if (data.length === 5000) {
+          if (isFirstInsert) {
+            await tx.$executeRaw`truncate client_claims_hourly`;
+            isFirstInsert = false;
+          }
+
+          await prismaService.client_claims_hourly.createMany({
+            data,
+          });
+
+          data.length = 0;
+        }
+      }
+
+      if (data.length > 0) {
+        await prismaService.client_claims_hourly.createMany({
+          data,
+        });
+      }
+    });
   }
 
   getFilledTables(): AggregationTable[] {
