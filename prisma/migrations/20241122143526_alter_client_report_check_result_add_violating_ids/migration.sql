54eeb4e82368dda6b7d3c839b5ee1216997f7022
Mateusz Mazur <mateusz.mazur@neti-soft.com>
2024-11-25 16:29:40 +0100

    FIL-438 Report checks - Storage Provider Distribution section - squash migrations

--- prisma/migrations/20241122143526_alter_client_report_check_result_add_violating_ids/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "client_report_check_result" ADD COLUMN     "violating_ids" INTEGER[];
--- prisma/migrations/20241122143638_alter_client_report_check_result_alter_violating_ids/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "client_report_check_result" ALTER COLUMN "violating_ids" SET DATA TYPE TEXT[];
