45e0f79f9cb90c8f3154b82bcf58db6558b67fd2
pingwindyktator <pingwindyktator@pm.me>
2024-12-03 18:26:28 +0100

    Add better error handling and healtcheck for client report generation

--- src/jobs/client-report-generator-job/client-report-generator-job.service.ts
+++ src/jobs/client-report-generator-job/client-report-generator-job.service.ts
@@ -3,44 +3,104 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { AllocatorTechService } from '../../service/allocator-tech/allocator-tech.service';
 import { ClientReportService } from '../../service/client-report/client-report.service';
 import { LotusApiService } from '../../service/lotus-api/lotus-api.service';
+import { AllocatorTechApplicationsResponse } from '../../service/allocator-tech/types.allocator-tech';
+import {
+  HealthCheckError,
+  HealthIndicator,
+  HealthIndicatorResult,
+} from '@nestjs/terminus';
 
 @Injectable()
-export class ClientReportGeneratorJobService {
+export class ClientReportGeneratorJobService extends HealthIndicator {
   private readonly logger = new Logger(ClientReportGeneratorJobService.name);
+  private lastRun: Date = null;
+  private lastRunApplications = 0;
+  private lastRunFails = 0;
+  private healthy = true;
 
   constructor(
     private readonly allocatorTechService: AllocatorTechService,
     private readonly clientReportService: ClientReportService,
     private readonly lotusApiService: LotusApiService,
-  ) {}
+  ) {
+    super();
+  }
+
+  async isHealthy(): Promise<HealthIndicatorResult> {
+    const result = this.getStatus('client-report-generator', this.healthy, {
+      lastRun: this.lastRun,
+      lastRunApplications: this.lastRunApplications,
+      lastRunFails: this.lastRunFails,
+    });
+
+    if (this.healthy) return result;
+    throw new HealthCheckError('Healthcheck failed', result);
+  }
+
+  private async _generateClientReport(
+    application: AllocatorTechApplicationsResponse,
+  ) {
+    // find Filecoin Client ID
+    const filecoinId = await this.lotusApiService.getFilecoinClientId(
+      application[0].ID,
+    );
+
+    if (!filecoinId) {
+      throw new Error(`Filecoin Id not found`);
+    }
+
+    if (!(await this.clientReportService.generateReport(filecoinId))) {
+      throw new Error(`client not found`);
+    }
+  }
+
+  private async _runClientReportGeneration() {
+    const applications = await this.allocatorTechService.getApplications();
+    let fails = 0;
+
+    for (const [i, application] of applications.entries()) {
+      try {
+        this.logger.debug(
+          `Starting generation of Client Report for application #${application[0].ID}`,
+        );
+
+        await this._generateClientReport(application);
+      } catch (err) {
+        fails++;
+        this.logger.error(
+          `Error during generation of Client Report for application #${application[0].ID}: ${err}`,
+          err,
+        );
+      }
+
+      if (i > 0 && i % 50 === 0) {
+        await new Promise((resolve) => setTimeout(resolve, 1000 * 60)); // 1 minute
+      }
+    }
+
+    return { applications: applications.length, fails: fails };
+  }
 
   @Cron(CronExpression.EVERY_DAY_AT_3AM)
   async runClientReportGenerationJob() {
-    this.logger.debug('Starting Client Reports generation');
-    const applications = await this.allocatorTechService.getApplications();
-    for (let i = 0; i < applications.length; i++) {
-      const application = applications[i];
-      this.logger.debug(
-        `Starting generation of Client Report for application #${application[0].ID}`,
+    try {
+      this.logger.log('Starting Client Reports generation');
+      this.lastRun = new Date();
+
+      const { applications, fails } = await this._runClientReportGeneration();
+
+      this.lastRunApplications = applications;
+      this.lastRunFails = fails;
+      this.healthy = true;
+      this.logger.log(
+        `Finishing Client Reports generation. Fails: ${fails} / ${applications}`,
       );
-
-      // find Filecoin Client ID
-      const filecoinId = await this.lotusApiService.getFilecoinClientId(
-        application[0].ID,
+    } catch (err) {
+      this.healthy = false;
+      this.logger.error(
+        `Error during Client Reports generation job: ${err}`,
+        err,
       );
-      if (!filecoinId) {
-        this.logger.debug(
-          `Filecoin Id not found for address #${application[0].ID}`,
-        );
-        continue;
-      }
-
-      await this.clientReportService.generateReport(filecoinId);
-
-      if (i > 0 && i % 50 === 0) {
-        await new Promise((resolve) => setTimeout(resolve, 1000 * 60));
-      }
     }
-    this.logger.debug('Finishing Client Reports generation');
   }
 }
