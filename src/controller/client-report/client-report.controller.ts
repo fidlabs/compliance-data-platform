2d6f118cd5e0f86e4e78912a9f9762ee46de5173
pingwindyktator <pingwindyktator@pm.me>
2024-12-03 10:54:41 +0100

    Fix clientId validation for report generation

--- src/controller/client-report/client-report.controller.ts
+++ src/controller/client-report/client-report.controller.ts
@@ -6,23 +6,52 @@ import {
   Post,
 } from '@nestjs/common';
 import { ClientReportService } from '../../service/client-report/client-report.service';
-import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 
 @Controller('clientReport')
 export class ClientReportController {
   constructor(private readonly clientReportsService: ClientReportService) {}
 
   @Get(':client')
+  @ApiOperation({
+    summary: 'Get list of client reports',
+  })
+  @ApiOkResponse({
+    description: 'List of client reports',
+    type: null,
+  })
   async getClientReports(@Param('client') client: string) {
     return await this.clientReportsService.getClientReports(client);
   }
 
   @Get(':client/latest')
+  @ApiOperation({
+    summary: 'Get latest client report',
+  })
+  @ApiOkResponse({
+    description: 'Client report',
+    type: null,
+  })
   async getClientReport(@Param('client') client: string) {
-    return await this.clientReportsService.getClientLatestReport(client);
+    const report =
+      await this.clientReportsService.getClientLatestReport(client);
+    if (!report) throw new NotFoundException();
+
+    return report;
   }
 
   @Get(':client/:id')
+  @ApiOperation({
+    summary: 'Get client report by id',
+  })
+  @ApiOkResponse({
+    description: 'Client report',
+    type: null,
+  })
   async getClientReportById(
     @Param('client') client: string,
     @Param('id') id: bigint,
@@ -38,10 +67,13 @@ export class ClientReportController {
     summary: 'Generate report for a given client',
   })
   @ApiCreatedResponse({
-    description: 'Empty response',
+    description: 'Client report',
     type: null,
   })
   async generateClientReport(@Param('client') client: string) {
-    await this.clientReportsService.generateReport(client);
+    const report = await this.clientReportsService.generateReport(client);
+    if (!report) throw new NotFoundException();
+
+    return report;
   }
 }
