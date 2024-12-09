8d84692d4904ec98cc0a886381bd12d4a834472f
pingwindyktator <pingwindyktator@pm.me>
2024-12-09 14:08:02 +0100

    Add compliance report CRUD

--- src/controller/compliance-report/compliance-report.controller.ts
+++ src/controller/compliance-report/compliance-report.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Param, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { ComplianceReportService } from '../../service/compliance-report/compliance-report.service';
 
 @Controller('complianceReport')
@@ -8,9 +12,52 @@ export class ComplianceReportController {
     private readonly complianceReportService: ComplianceReportService,
   ) {}
 
+  @Get(':allocatorAddress')
+  @ApiOperation({
+    summary: 'Get list of compliance reports',
+  })
+  @ApiOkResponse({
+    description: 'List of compliance reports',
+    type: null,
+  })
+  async getComplianceReports(
+    @Param('allocatorAddress') allocatorAddress: string,
+  ) {
+    //
+  }
+
+  @Get(':allocatorAddress/latest')
+  @ApiOperation({
+    summary: 'Get latest compliance report',
+  })
+  @ApiOkResponse({
+    description: 'Compliance Report',
+    type: null,
+  })
+  async getComplianceReport(
+    @Param('allocatorAddress') allocatorAddress: string,
+  ) {
+    //
+  }
+
+  @Get(':allocatorAddress/:id')
+  @ApiOperation({
+    summary: 'Get compliance report by id',
+  })
+  @ApiOkResponse({
+    description: 'Compliance Report',
+    type: null,
+  })
+  async getComplianceReportById(
+    @Param('allocatorAddress') allocatorAddress: string,
+    @Param('id') id: bigint,
+  ) {
+    //
+  }
+
   @Post(':allocatorAddress')
   @ApiOperation({
-    summary: 'Generate Compliance report for a given Allocator',
+    summary: 'Generate compliance report for a given allocator',
   })
   @ApiCreatedResponse({
     description: 'Compliance Report',
