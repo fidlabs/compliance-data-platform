b4e5900342d081e2f98c5ccc1227b3dbf428d192
lukas <lukasz.wal@neti-soft.com>
2024-12-27 15:17:24 +0100

    Metrics endpoint for Prometheus

--- src/common/prometheus/prometheus.controller.ts
+++ src/common/prometheus/prometheus.controller.ts
@@ -1,7 +1,14 @@
-import { PrometheusController as PrometheusControllerSource } from '@willsoto/nestjs-prometheus';
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
+import { PrometheusController } from '@willsoto/nestjs-prometheus';
+import { MetricsAuthGuard } from '../guards/metrics.guard';
 
 @Controller()
 @ApiExcludeController()
-export class PrometheusMetricController extends PrometheusControllerSource {}
+export class PrometheusMetricController extends PrometheusController {
+  @Get()
+  @UseGuards(MetricsAuthGuard)
+  async index(@Res({ passthrough: true }) response: Response) {
+    return super.index(response);
+  }
+}
