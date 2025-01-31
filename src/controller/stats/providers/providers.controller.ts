a811d830f53364a1c89b7a38f11fc71fbdb8ded7
pingwindyktator <pingwindyktator@pm.me>
2025-01-31 16:07:53 +0100

    Review fixes

--- src/controller/stats/providers/providers.controller.ts
+++ src/controller/stats/providers/providers.controller.ts
@@ -2,11 +2,28 @@ import { Controller, Get } from '@nestjs/common';
 import { ProviderService } from '../../../service/provider/provider.service';
 import { HistogramWeekResponseDto } from '../../../types/histogramWeek.response.dto';
 import { RetrievabilityWeekResponseDto } from '../../../types/retrievabilityWeekResponse.dto';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { AggregatedProvidersIPNIReportingStatus } from '../../../service/ipni-misreporting-checker/types.ipni-misreporting-checker';
+import { IpniMisreportingCheckerService } from '../../../service/ipni-misreporting-checker/ipni-misreporting-checker.service';
 
 @Controller('stats/providers')
 export class ProvidersController {
-  constructor(private readonly providerService: ProviderService) {}
+  constructor(
+    private readonly providerService: ProviderService,
+    private readonly ipniMisreportingCheckerService: IpniMisreportingCheckerService,
+  ) {}
+
+  @Get('/aggregated-ipni-status')
+  @ApiOperation({
+    summary: 'Get aggregated storage providers IPNI reporting status',
+  })
+  @ApiOkResponse({
+    description: 'Aggregated storage providers IPNI reporting status',
+    type: AggregatedProvidersIPNIReportingStatus,
+  })
+  async getAggregatedProvidersIPNIReportingStatus(): Promise<AggregatedProvidersIPNIReportingStatus> {
+    return await this.ipniMisreportingCheckerService.getAggregatedProvidersReportingStatus();
+  }
 
   @Get('clients')
   @ApiOkResponse({ type: HistogramWeekResponseDto })
