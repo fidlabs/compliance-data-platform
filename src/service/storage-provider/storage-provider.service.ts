98a0749b3a5ed28f3c53fb24f09bba1d6aecd951
pingwindyktator <pingwindyktator@pm.me>
2025-02-04 19:52:34 +0100

    Refactor allocatorService && storageProviderService

--- src/service/storage-provider/storage-provider.service.ts
+++ src/service/storage-provider/storage-provider.service.ts
@@ -14,6 +14,8 @@ import { DateTime } from 'luxon';
 import { Prisma } from 'prisma/generated/client';
 import { modelName } from 'src/utils/prisma.helper';
 import { HistogramWeekResponseDto } from '../../types/histogramWeek.response.dto';
+import { ProviderComplianceScoreRange } from '../../types/providerComplianceScoreRange';
+import { SpsComplianceWeekDto } from '../../types/spsComplianceWeek.dto';
 
 @Injectable()
 export class StorageProviderService {
@@ -22,7 +24,7 @@ export class StorageProviderService {
     private readonly histogramHelper: HistogramHelper,
   ) {}
 
-  async getProviderClients(
+  public async getProviderClients(
     isAccumulative: boolean,
   ): Promise<HistogramWeekResponseDto> {
     const clientProviderDistributionWeeklyTable = isAccumulative
@@ -48,7 +50,7 @@ export class StorageProviderService {
     );
   }
 
-  async getProviderBiggestClientDistribution(
+  public async getProviderBiggestClientDistribution(
     isAccumulative: boolean,
   ): Promise<HistogramWeekResponseDto> {
     const clientProviderDistributionWeeklyTable = isAccumulative
@@ -74,7 +76,7 @@ export class StorageProviderService {
     );
   }
 
-  async getProviderRetrievability(
+  public async getProviderRetrievability(
     isAccumulative: boolean,
   ): Promise<RetrievabilityWeekResponseDto> {
     const providersWeeklyTable = isAccumulative
@@ -106,6 +108,181 @@ export class StorageProviderService {
     return RetrievabilityWeekResponseDto.of(
       providerCountAndAverageSuccessRate[0].averageSuccessRate,
       weeklyHistogramResult,
+    );
+  }
+
+  public async getWeekProvidersForClients(
+    week: Date,
+    isAccumulative: boolean,
+    clients: string[],
+  ): Promise<string[]> {
+    return (
+      await (
+        (isAccumulative
+          ? this.prismaService.client_provider_distribution_weekly_acc
+          : this.prismaService.client_provider_distribution_weekly) as any
+      ).findMany({
+        where: {
+          week: week,
+          client: {
+            in: clients,
+          },
+        },
+        select: {
+          provider: true,
+        },
+      })
+    ).map((p) => p.provider);
+  }
+
+  public async getWeekProviders(week: Date, isAccumulative: boolean) {
+    return (
+      (isAccumulative
+        ? this.prismaService.providers_weekly_acc
+        : this.prismaService.providers_weekly) as any
+    ).findMany({
+      where: {
+        week: week,
+      },
+    });
+  }
+
+  public async getWeeksTracked(isAccumulative: boolean): Promise<Date[]> {
+    return (
+      await (
+        (isAccumulative
+          ? this.prismaService.providers_weekly_acc
+          : this.prismaService.providers_weekly) as any
+      ).findMany({
+        distinct: ['week'],
+        select: {
+          week: true,
+        },
+        orderBy: {
+          week: 'asc',
+        },
+      })
+    ).map((p) => p.week);
+  }
+
+  public async getWeekAverageProviderRetrievability(
+    week: Date,
+    isAccumulative: boolean,
+  ): Promise<number> {
+    return (
+      await (
+        (isAccumulative
+          ? this.prismaService.providers_weekly_acc
+          : this.prismaService.providers_weekly) as any
+      ).aggregate({
+        _avg: {
+          avg_retrievability_success_rate: true,
+        },
+        where: {
+          week: week,
+        },
+      })
+    )._avg.avg_retrievability_success_rate;
+  }
+
+  public getWeekProviderComplianceScore(
+    providerWeekly: {
+      avg_retrievability_success_rate: number;
+      num_of_clients: number;
+      biggest_client_total_deal_size: bigint;
+      total_deal_size: bigint | null;
+      provider: string;
+    },
+    thisWeekAverageRetrievability: number,
+  ): {
+    complianceScore: number;
+    provider: string;
+  } {
+    let complianceScore = 0;
+
+    // TODO when business is ready let's switch to using http success rate.
+    // Question - do we make a cutoff date for this? (like use normal rate
+    // till 25w4 and http rate after that)?
+    if (
+      providerWeekly.avg_retrievability_success_rate >
+      thisWeekAverageRetrievability
+    )
+      complianceScore++;
+
+    if (providerWeekly.num_of_clients > 3) complianceScore++;
+
+    if (
+      providerWeekly.biggest_client_total_deal_size * 100n <=
+      30n * providerWeekly.total_deal_size
+    )
+      complianceScore++;
+
+    return {
+      provider: providerWeekly.provider,
+      complianceScore: complianceScore,
+    };
+  }
+
+  public getCompliantProvidersPercentage(
+    weekProvidersCompliance: {
+      complianceScore: number;
+      provider: string;
+    }[],
+    providers: string[],
+  ): {
+    compliantSpsPercentage: number;
+    partiallyCompliantSpsPercentage: number;
+    nonCompliantSpsPercentage: number;
+  } {
+    return {
+      compliantSpsPercentage: this._getCompliantProvidersPercentage(
+        weekProvidersCompliance,
+        providers,
+        ProviderComplianceScoreRange.Compliant,
+      ),
+      partiallyCompliantSpsPercentage: this._getCompliantProvidersPercentage(
+        weekProvidersCompliance,
+        providers,
+        ProviderComplianceScoreRange.PartiallyCompliant,
+      ),
+      nonCompliantSpsPercentage: this._getCompliantProvidersPercentage(
+        weekProvidersCompliance,
+        providers,
+        ProviderComplianceScoreRange.NonCompliant,
+      ),
+    };
+  }
+
+  private _getCompliantProvidersPercentage(
+    weekProvidersCompliance: {
+      complianceScore: number;
+      provider: string;
+    }[],
+    providers: string[],
+    validComplianceScore: ProviderComplianceScoreRange,
+  ): number {
+    const validComplianceScores: number[] = [];
+
+    switch (validComplianceScore) {
+      case ProviderComplianceScoreRange.NonCompliant:
+        validComplianceScores.push(0);
+        break;
+      case ProviderComplianceScoreRange.PartiallyCompliant:
+        validComplianceScores.push(1, 2);
+        break;
+      case ProviderComplianceScoreRange.Compliant:
+        validComplianceScores.push(3);
+        break;
+    }
+
+    return (
+      (100 *
+        weekProvidersCompliance.filter(
+          (p) =>
+            providers.includes(p.provider) &&
+            validComplianceScores.includes(p.complianceScore),
+        ).length) /
+      providers.length
     );
   }
 }
