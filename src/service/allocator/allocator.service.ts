20080cc02f2b64a6ec3c973324ef7e438fddf99b
pingwindyktator <pingwindyktator@pm.me>
2025-02-04 19:52:34 +0100

    Refactor allocatorService && storageProviderService

--- src/service/allocator/allocator.service.ts
+++ src/service/allocator/allocator.service.ts
@@ -19,15 +19,17 @@ import { HistogramWeekResponseDto } from '../../types/histogramWeek.response.dto
 import { SpsComplianceSingleAllocatorDto } from 'src/types/spsComplianceSingleAllocator.dto';
 import { SpsComplianceHistogramWeekDto } from 'src/types/spsComplianceHistogramWeek.dto';
 import { SpsComplianceHistogramWeekResponseDto } from 'src/types/spsComplianceHistogramWeekResponse.dto';
+import { StorageProviderService } from '../storage-provider/storage-provider.service';
 
 @Injectable()
 export class AllocatorService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly histogramHelper: HistogramHelper,
+    private readonly storageProviderService: StorageProviderService,
   ) {}
 
-  async getAllocatorRetrievability(
+  public async getAllocatorRetrievability(
     isAccumulative: boolean,
   ): Promise<RetrievabilityWeekResponseDto> {
     const lastWeek = DateTime.now()
@@ -36,23 +38,10 @@ export class AllocatorService {
       .startOf('week')
       .toJSDate();
 
-    const averageSuccessRate = isAccumulative
-      ? await this.prismaService.allocators_weekly_acc.aggregate({
-          _avg: {
-            avg_weighted_retrievability_success_rate: true,
-          },
-          where: {
-            week: lastWeek,
-          },
-        })
-      : await this.prismaService.allocators_weekly.aggregate({
-          _avg: {
-            avg_weighted_retrievability_success_rate: true,
-          },
-          where: {
-            week: lastWeek,
-          },
-        });
+    const averageSuccessRate = await this.getWeekAverageAllocatorRetrievability(
+      lastWeek,
+      isAccumulative,
+    );
 
     const clientAllocatorDistributionWeeklyTable = isAccumulative
       ? Prisma.ModelName.client_allocator_distribution_weekly_acc
@@ -78,12 +67,12 @@ export class AllocatorService {
       );
 
     return RetrievabilityWeekResponseDto.of(
-      averageSuccessRate._avg.avg_weighted_retrievability_success_rate * 100,
+      averageSuccessRate * 100,
       weeklyHistogramResult,
     );
   }
 
-  async getAllocatorBiggestClientDistribution(
+  public async getAllocatorBiggestClientDistribution(
     isAccumulative: boolean,
   ): Promise<HistogramWeekResponseDto> {
     const clientAllocatorDistributionWeeklyTable = isAccumulative
@@ -109,39 +98,25 @@ export class AllocatorService {
     );
   }
 
-  async getAllocatorSpsComplianceHistogram(
+  public async getAllocatorSpsComplianceHistogram(
     isAccumulative: boolean,
   ): Promise<SpsComplianceHistogramWeekResponseDto> {
-    const allocatorCount = (
-      isAccumulative
-        ? await this.prismaService.allocators_weekly_acc.findMany({
-            distinct: ['allocator'],
-            select: {
-              allocator: true,
-            },
-          })
-        : await this.prismaService.allocators_weekly.findMany({
-            distinct: ['allocator'],
-            select: {
-              allocator: true,
-            },
-          })
-    ).length;
+    const allocatorCount = await this.getAllocatorCount(isAccumulative);
 
     const { results } = await this.getAllocatorSpsCompliance(isAccumulative);
 
     return new SpsComplianceHistogramWeekResponseDto([
-      await this.calculateSpsComplianceWeekDto(
+      await this.calculateSpsComplianceWeek(
         results,
         allocatorCount,
         ProviderComplianceScoreRange.NonCompliant,
       ),
-      await this.calculateSpsComplianceWeekDto(
+      await this.calculateSpsComplianceWeek(
         results,
         allocatorCount,
         ProviderComplianceScoreRange.PartiallyCompliant,
       ),
-      await this.calculateSpsComplianceWeekDto(
+      await this.calculateSpsComplianceWeek(
         results,
         allocatorCount,
         ProviderComplianceScoreRange.Compliant,
@@ -149,112 +124,38 @@ export class AllocatorService {
     ]);
   }
 
-  async getAllocatorSpsCompliance(
+  // TODO cache?
+  public async getAllocatorSpsCompliance(
     isAccumulative: boolean,
   ): Promise<SpsComplianceWeekResponseDto> {
-    const weeks = isAccumulative
-      ? await this.prismaService.providers_weekly_acc
-          .findMany({
-            distinct: ['week'],
-            select: {
-              week: true,
-            },
-            orderBy: {
-              week: 'asc',
-            },
-          })
-          .then((r) => r.map((p) => p.week))
-      : await this.prismaService.providers_weekly
-          .findMany({
-            distinct: ['week'],
-            select: {
-              week: true,
-            },
-            orderBy: {
-              week: 'asc',
-            },
-          })
-          .then((r) => r.map((p) => p.week));
+    const weeks =
+      await this.storageProviderService.getWeeksTracked(isAccumulative);
 
-    const calculationResults: SpsComplianceWeekDto[] = [];
+    const result: SpsComplianceWeekDto[] = [];
 
     for (const week of weeks) {
-      const thisWeekAverageRetrievability = isAccumulative
-        ? await this.prismaService.providers_weekly_acc.aggregate({
-            _avg: {
-              avg_retrievability_success_rate: true,
-            },
-            where: {
-              week: week,
-            },
-          })
-        : await this.prismaService.providers_weekly.aggregate({
-            _avg: {
-              avg_retrievability_success_rate: true,
-            },
-            where: {
-              week: week,
-            },
-          });
+      const thisWeekAverageRetrievability =
+        await this.storageProviderService.getWeekAverageProviderRetrievability(
+          week,
+          isAccumulative,
+        );
 
-      const weekProviders = isAccumulative
-        ? await this.prismaService.providers_weekly_acc.findMany({
-            where: {
-              week: week,
-            },
-          })
-        : await this.prismaService.providers_weekly.findMany({
-            where: {
-              week: week,
-            },
-          });
+      const weekProviders = await this.storageProviderService.getWeekProviders(
+        week,
+        isAccumulative,
+      );
 
       const weekProvidersCompliance = weekProviders.map((wp) => {
-        let complianceScore = 0;
-        // TODO when business is ready let's switch to using http success rate.
-        // Question - do we make a cutoff date for this? (like use normal rate
-        // till 25w4 and http rate after that)?
-        if (
-          wp.avg_retrievability_success_rate >
-          thisWeekAverageRetrievability._avg.avg_retrievability_success_rate
-        )
-          complianceScore++;
-        if (wp.num_of_clients > 3) complianceScore++;
-        if (
-          wp.biggest_client_total_deal_size * 100n <=
-          30n * wp.total_deal_size
-        )
-          complianceScore++;
-
-        return {
-          provider: wp.provider,
-          complianceScore: complianceScore,
-        };
+        return this.storageProviderService.getWeekProviderComplianceScore(
+          wp,
+          thisWeekAverageRetrievability,
+        );
       });
 
-      const weekAllocatorsWithClients = isAccumulative
-        ? await this.prismaService.client_allocator_distribution_weekly_acc.findMany(
-            {
-              where: {
-                week: week,
-              },
-              select: {
-                client: true,
-                allocator: true,
-              },
-            },
-          )
-        : await this.prismaService.client_allocator_distribution_weekly.findMany(
-            {
-              where: {
-                week: week,
-              },
-              select: {
-                client: true,
-                allocator: true,
-              },
-            },
-          );
+      const weekAllocatorsWithClients = await this.getWeekAllocatorsWithClients(
+        week,
+        isAccumulative,
+      );
 
       const byAllocators = groupBy(
         weekAllocatorsWithClients,
@@ -262,71 +163,93 @@ export class AllocatorService {
       );
 
       const weekResult: SpsComplianceSingleAllocatorDto[] = [];
-      for (const allocator in byAllocators) {
-        const clients = byAllocators[allocator].map((p) => p.client);
 
-        const providersRaw = isAccumulative
-          ? await this.prismaService.client_provider_distribution_weekly_acc
-              .findMany({
-                where: {
-                  week: week,
-                  client: {
-                    in: clients,
-                  },
-                },
-                select: {
-                  provider: true,
-                },
-              })
-              .then((r) => r.map((p) => p.provider))
-          : await this.prismaService.client_provider_distribution_weekly
-              .findMany({
-                where: {
-                  week: week,
-                  client: {
-                    in: clients,
-                  },
-                },
-                select: {
-                  provider: true,
-                },
-              })
-              .then((r) => r.map((p) => p.provider));
-        const providers = [...new Set(providersRaw)];
+      for (const allocator in byAllocators) {
+        const _weekProvidersForAllocator =
+          await this.storageProviderService.getWeekProvidersForClients(
+            week,
+            isAccumulative,
+            byAllocators[allocator].map((p) => p.client),
+          );
+
+        const weekProvidersForAllocator = [
+          ...new Set(_weekProvidersForAllocator),
+        ];
 
         weekResult.push({
           id: allocator,
-          compliantSpsPercentage: this.getAllocatorCompliantProvidersPercentage(
+          ...this.storageProviderService.getCompliantProvidersPercentage(
             weekProvidersCompliance,
-            providers,
-            ProviderComplianceScoreRange.Compliant,
+            weekProvidersForAllocator,
           ),
-          partiallyCompliantSpsPercentage:
-            this.getAllocatorCompliantProvidersPercentage(
-              weekProvidersCompliance,
-              providers,
-              ProviderComplianceScoreRange.PartiallyCompliant,
-            ),
-          nonCompliantSpsPercentage:
-            this.getAllocatorCompliantProvidersPercentage(
-              weekProvidersCompliance,
-              providers,
-              ProviderComplianceScoreRange.NonCompliant,
-            ),
         });
       }
 
-      calculationResults.push({
+      result.push({
         week: week,
         allocators: weekResult,
         total: weekResult.length,
       });
     }
 
-    return new SpsComplianceWeekResponseDto(calculationResults);
+    return new SpsComplianceWeekResponseDto(result);
   }
 
-  private async calculateSpsComplianceWeekDto(
+  private async getAllocatorCount(isAccumulative: boolean): Promise<number> {
+    return (
+      await (
+        (isAccumulative
+          ? this.prismaService.allocators_weekly_acc
+          : this.prismaService.allocators_weekly) as any
+      ).findMany({
+        distinct: ['allocator'],
+        select: {
+          allocator: true,
+        },
+      })
+    ).length;
+  }
+
+  private async getWeekAverageAllocatorRetrievability(
+    week: Date,
+    isAccumulative: boolean,
+  ): Promise<number> {
+    return (
+      await (
+        (isAccumulative
+          ? this.prismaService.allocators_weekly_acc
+          : this.prismaService.allocators_weekly) as any
+      ).aggregate({
+        _avg: {
+          avg_weighted_retrievability_success_rate: true,
+        },
+        where: {
+          week: week,
+        },
+      })
+    )._avg.avg_weighted_retrievability_success_rate;
+  }
+
+  private async getWeekAllocatorsWithClients(
+    week: Date,
+    isAccumulative: boolean,
+  ) {
+    return (
+      (isAccumulative
+        ? this.prismaService.client_allocator_distribution_weekly_acc
+        : this.prismaService.client_allocator_distribution_weekly) as any
+    ).findMany({
+      where: {
+        week: week,
+      },
+      select: {
+        client: true,
+        allocator: true,
+      },
+    });
+  }
+
+  private async calculateSpsComplianceWeek(
     calculationResults: SpsComplianceWeekDto[],
     allocatorCount: number,
     providerComplianceScoreRange: ProviderComplianceScoreRange,
@@ -353,12 +276,14 @@ export class AllocatorService {
     week: Date;
   }[] {
     let valueFromExclusive = -5;
+
     const result: {
       valueFromExclusive: number | null;
       valueToInclusive: number | null;
       count: number | null;
       week: Date;
     }[] = [];
+
     do {
       result.push(
         ...unsortedResults.map((r) => {
@@ -396,43 +321,12 @@ export class AllocatorService {
     switch (providerComplianceScoreRange) {
       case ProviderComplianceScoreRange.NonCompliant:
         return result.nonCompliantSpsPercentage;
+
       case ProviderComplianceScoreRange.PartiallyCompliant:
         return result.partiallyCompliantSpsPercentage;
+
       case ProviderComplianceScoreRange.Compliant:
         return result.compliantSpsPercentage;
     }
-  }
-
-  private getAllocatorCompliantProvidersPercentage(
-    weekProvidersCompliance: {
-      complianceScore: number;
-      provider: string;
-    }[],
-    providers: string[],
-    complianceScore: ProviderComplianceScoreRange,
-  ) {
-    const validComplianceScores: number[] = [];
-
-    switch (complianceScore) {
-      case ProviderComplianceScoreRange.NonCompliant:
-        validComplianceScores.push(0);
-        break;
-      case ProviderComplianceScoreRange.PartiallyCompliant:
-        validComplianceScores.push(1, 2);
-        break;
-      case ProviderComplianceScoreRange.Compliant:
-        validComplianceScores.push(3);
-        break;
-    }
-
-    return (
-      (100 *
-        weekProvidersCompliance.filter(
-          (p) =>
-            providers.includes(p.provider) &&
-            validComplianceScores.includes(p.complianceScore),
-        ).length) /
-      providers.length
-    );
   }
 }
