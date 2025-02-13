ae6bd42744f119728be36ac1c0f365d43b306826
pingwindyktator <pingwindyktator@pm.me>
2025-02-13 15:56:21 +0100

    Reformat .sql files again

--- prisma/sql/getAllocatorsWeekly.sql
+++ prisma/sql/getAllocatorsWeekly.sql
@@ -1,25 +1,26 @@
-with allocator_retrievability as (select week                                                                                                    as "week",
-                                         allocator                                                                                               as "allocator",
-                                         sum(cpd.total_deal_size * coalesce(avg_retrievability_success_rate, 0)) / sum(cpd.total_deal_size)      as avg_weighted_retrievability_success_rate,
-                                         sum(cpd.total_deal_size * coalesce(avg_retrievability_success_rate_http, 0)) / sum(cpd.total_deal_size) as avg_weighted_retrievability_success_rate_http
-                                  from client_allocator_distribution_weekly
-                                           inner join client_provider_distribution_weekly as cpd using (client, week)
-                                           left join providers_weekly using (provider, week)
-                                  group by week, allocator),
-     allocator_stats as (select week                    as "week",
-                                allocator               as "allocator",
-                                count(*)                as num_of_clients,
-                                max(sum_of_allocations) as biggest_client_sum_of_allocations,
-                                sum(sum_of_allocations) as total_sum_of_allocations
-                         from client_allocator_distribution_weekly
-                         group by week, allocator)
-select week                                                       as "week",
-       allocator                                                  as "allocator",
-       num_of_clients::int,
-       biggest_client_sum_of_allocations::bigint,
-       total_sum_of_allocations::bigint,
-       coalesce(avg_weighted_retrievability_success_rate, 0)      as avg_weighted_retrievability_success_rate,
-       coalesce(avg_weighted_retrievability_success_rate_http, 0) as avg_weighted_retrievability_success_rate_http
-from allocator_stats
-         left join allocator_retrievability
-                   using (week, allocator);
+with "allocator_retrievability" as (select "week"                                                                                                            as "week",
+                                           "allocator"                                                                                                       as "allocator",
+                                           sum("cpd"."total_deal_size" * coalesce("avg_retrievability_success_rate", 0)) / sum("cpd"."total_deal_size")      as "avg_weighted_retrievability_success_rate",
+                                           sum("cpd"."total_deal_size" * coalesce("avg_retrievability_success_rate_http", 0)) /
+                                           sum("cpd"."total_deal_size")                                                                                      as "avg_weighted_retrievability_success_rate_http"
+                                    from "client_allocator_distribution_weekly"
+                                             inner join "client_provider_distribution_weekly" as "cpd" using ("client", "week")
+                                             left join "providers_weekly" using ("provider", "week")
+                                    group by "week", "allocator"),
+     "allocator_stats" as (select "week"                    as "week",
+                                  "allocator"               as "allocator",
+                                  count(*)                  as "num_of_clients",
+                                  max("sum_of_allocations") as "biggest_client_sum_of_allocations",
+                                  sum("sum_of_allocations") as "total_sum_of_allocations"
+                           from "client_allocator_distribution_weekly"
+                           group by "week", "allocator")
+select "week"                                                       as "week",
+       "allocator"                                                  as "allocator",
+       "num_of_clients"::int                                        as "num_of_clients",
+       "biggest_client_sum_of_allocations"::bigint                  as "biggest_client_sum_of_allocations",
+       "total_sum_of_allocations"::bigint                           as "total_sum_of_allocations",
+       coalesce("avg_weighted_retrievability_success_rate", 0)      as "avg_weighted_retrievability_success_rate",
+       coalesce("avg_weighted_retrievability_success_rate_http", 0) as "avg_weighted_retrievability_success_rate_http"
+from "allocator_stats"
+         left join "allocator_retrievability"
+                   using ("week", "allocator");
--- prisma/sql/getProvidersWeeklyAcc.sql
+++ prisma/sql/getProvidersWeeklyAcc.sql
@@ -1,19 +1,19 @@
-with provider_retrievability_weekly as (select date_trunc('week', date)                                       as week,
-                                               provider,
-                                               sum(total)                                                     as total,
-                                               sum(successful)                                                as successful,
-                                               sum(successful::float8) / sum(total::float8)                   as success_rate,
-                                               sum(coalesce(successful_http, 0))                              as successful_http,
-                                               sum(coalesce(successful_http, 0)::float8) / sum(total::float8) as success_rate_http
-                                        from provider_retrievability_daily
-                                        group by week, provider)
-select week,
-       provider,
-       count(*)::int                       as num_of_clients,
-       max(total_deal_size)::bigint        as biggest_client_total_deal_size,
-       sum(total_deal_size)::bigint        as total_deal_size,
-       max(coalesce(success_rate, 0))      as avg_retrievability_success_rate,
-       max(coalesce(success_rate_http, 0)) as avg_retrievability_success_rate_http
-from client_provider_distribution_weekly_acc as cpdwa
-         left join provider_retrievability_weekly as prw using (week, provider)
-group by week, provider;
+with "provider_retrievability_weekly" as (select date_trunc('week', "date")                                         as "week",
+                                                 "provider"                                                         as "provider",
+                                                 sum("total")                                                       as "total",
+                                                 sum("successful")                                                  as "successful",
+                                                 sum("successful"::float8) / sum("total"::float8)                   as "success_rate",
+                                                 sum(coalesce("successful_http", 0))                                as "successful_http",
+                                                 sum(coalesce("successful_http", 0)::float8) / sum("total"::float8) as "success_rate_http"
+                                          from "provider_retrievability_daily"
+                                          group by "week", "provider")
+select "week"                                as "week",
+       "provider"                            as "provider",
+       count(*)::int                         as "num_of_clients",
+       max("total_deal_size")::bigint        as "biggest_client_total_deal_size",
+       sum("total_deal_size")::bigint        as "total_deal_size",
+       max(coalesce("success_rate", 0))      as "avg_retrievability_success_rate",
+       max(coalesce("success_rate_http", 0)) as "avg_retrievability_success_rate_http"
+from "client_provider_distribution_weekly_acc" as "cpdwa"
+         left join "provider_retrievability_weekly" as "prw" using ("week", "provider")
+group by "week", "provider";
