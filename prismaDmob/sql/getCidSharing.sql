3524e9c284b5e4e98f2ddfdc615ec52a558c0fc4
pingwindyktator <pingwindyktator@pm.me>
2025-02-18 19:07:01 +0100

    Reformat dmob .sql files

--- prismaDmob/sql/getCidSharing.sql
+++ prismaDmob/sql/getCidSharing.sql
@@ -1,30 +1,20 @@
-with 
-    nv22_unified_verified_deal as (
-        select
-            "clientId",
-            "pieceCid",
-            "pieceSize"
-        from unified_verified_deal
-        where
-            "termStart" >= 3847920 -- nv22 start
-            and to_timestamp("termStart" * 30 + 1598306400) <= current_timestamp -- deals that didn't start yet
-    ),
-    cids as (
-        select distinct
-            "clientId",
-            "pieceCid"
-        from nv22_unified_verified_deal
-    )
-select 
-    'f0' || cids."clientId" as client,
-    'f0' || other_dc."clientId" as other_client,
-    sum(other_dc."pieceSize")::float as total_deal_size,
-    count(distinct other_dc."pieceCid")::int as unique_cid_count
-from cids
-join nv22_unified_verified_deal other_dc
-    on
-        cids."pieceCid" = other_dc."pieceCid"
-        and cids."clientId" != other_dc."clientId"
-group by
-    client,
-    other_client;
+with "nv22_unified_verified_deal" as (select "clientId"  as "clientId",
+                                             "pieceCid"  as "pieceCid",
+                                             "pieceSize" as "pieceSize"
+                                      from "unified_verified_deal"
+                                      where "termStart" >= 3847920 -- nv22 start
+                                        and to_timestamp("termStart" * 30 + 1598306400) <= current_timestamp -- deals that didn't start yet
+),
+     "cids" as (select distinct "clientId" as "clientId",
+                                "pieceCid" as "pieceCid"
+                from "nv22_unified_verified_deal")
+select 'f0' || "cids"."clientId"                          as "client",
+       'f0' || "other_dc"."clientId"                      as "other_client",
+       sum("other_dc"."pieceSize")::float                 as "total_deal_size",
+       count(distinct "other_dc"."pieceCid")::int         as "unique_cid_count"
+from "cids"
+         join "nv22_unified_verified_deal" "other_dc"
+              on
+                  "cids"."pieceCid" = "other_dc"."pieceCid"
+                      and "cids"."clientId" != "other_dc"."clientId"
+group by "client", "other_client";
--- prismaDmob/sql/getClientProviderDistributionWeekly.sql
+++ prismaDmob/sql/getClientProviderDistributionWeekly.sql
@@ -1,29 +1,20 @@
-with miner_pieces as (
-    select
-        date_trunc('week', to_timestamp("termStart" * 30 + 1598306400)) as week,
-        'f0' || "clientId"  as client,
-        'f0' || "providerId"  as provider,
-        "pieceCid",
-        sum("pieceSize") as total_deal_size,
-        min("pieceSize") as piece_size
-    from unified_verified_deal
-    where
-        "termStart" >= 3847920 -- nv22 start
-        and to_timestamp("termStart" * 30 + 1598306400) <= current_timestamp -- deals that didn't start yet
-    group by
-        week,
-        client,
-        provider,
-        "pieceCid"
-)
-select
-    week,
-    client,
-    provider,
-    sum(total_deal_size)::float as total_deal_size,
-    sum(piece_size)::float      as unique_data_size
-from  miner_pieces
-group by
-    week,
-    client,
-    provider;
+with "miner_pieces" as (select date_trunc('week', to_timestamp("termStart" * 30 + 1598306400))         as "week",
+                               'f0' || "clientId"                                                      as "client",
+                               'f0' || "providerId"                                                    as "provider",
+                               "pieceCid"                                                              as "pieceCid",
+                               sum("pieceSize")                                                        as "total_deal_size",
+                               min("pieceSize")                                                        as "piece_size"
+                        from "unified_verified_deal"
+                        where "termStart" >= 3847920                                           -- nv22 start
+                          and to_timestamp("termStart" * 30 + 1598306400) <= current_timestamp -- deals that didn't start yet
+                        group by "week",
+                                 "client",
+                                 "provider",
+                                 "pieceCid")
+select "week"                        as "week",
+       "client"                      as "client",
+       "provider"                    as "provider",
+       sum("total_deal_size")::float as "total_deal_size",
+       sum("piece_size")::float      as "unique_data_size"
+from "miner_pieces"
+group by "week", "client", "provider";
