c38190f8230f6dbd9a50596e1f85ecf73eee6a97
lukas <lukasz.wal@neti-soft.com>
2025-02-20 13:19:41 +0100

    Change the metrics for pg pool

--- src/prometheus/db-metrics/metrics.ts
+++ src/prometheus/db-metrics/metrics.ts
@@ -1,18 +1,23 @@
 import { makeGaugeProvider } from '@willsoto/nestjs-prometheus';
 
 export enum PgPoolCdpGaugeMetricsType {
-  PG_POOL_ACTIVE_CONNECTIONS_COUNT = 'pgpool_active_connections_count',
-  PG_POOL_ALL_CONNECTIONS_COUNT = 'pgpool_all_connections_count',
+  PG_POOL_CLIENT_EXIST_COUNT = 'pgpool_client_count',
+  PG_POOL_CLIENT_IDLE_COUNT = 'pgpool_client_idle_count',
+  PG_POOL_CLIENT_WAITING_COUNT = 'pgpool_client_waiting_count',
 }
 
 const pgPoolPrometheusGauges = [
   makeGaugeProvider({
-    name: PgPoolCdpGaugeMetricsType.PG_POOL_ACTIVE_CONNECTIONS_COUNT,
-    help: 'Number of active connections in the PostgreSQL pool',
+    name: PgPoolCdpGaugeMetricsType.PG_POOL_CLIENT_EXIST_COUNT,
+    help: 'The total number of clients existing within the pool.',
   }),
   makeGaugeProvider({
-    name: PgPoolCdpGaugeMetricsType.PG_POOL_ALL_CONNECTIONS_COUNT,
-    help: 'Number of all connections in the PostgreSQL pool',
+    name: PgPoolCdpGaugeMetricsType.PG_POOL_CLIENT_IDLE_COUNT,
+    help: 'The number of clients which are not checked out but are currently idle in the pool.',
+  }),
+  makeGaugeProvider({
+    name: PgPoolCdpGaugeMetricsType.PG_POOL_CLIENT_WAITING_COUNT,
+    help: 'The number of clients which are currently waiting to be checked out from the pool.',
   }),
 ];
 
