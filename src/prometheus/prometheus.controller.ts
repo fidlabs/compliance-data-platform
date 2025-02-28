d4f1155a2db9d4e9ad8107addf3a906377f25508
lukas <lukasz.wal@neti-soft.com>
2025-02-28 12:56:09 +0100

    Add separately metrics controller for nestjs-prometheus lib

--- src/prometheus/prometheus.controller.ts
+++ src/prometheus/prometheus.controller.ts
@@ -1,21 +1,12 @@
-import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
-import { PrometheusController } from '@willsoto/nestjs-prometheus';
 import { PrismaService } from 'src/db/prisma.service';
 import { PrometheusAuthGuard } from './guards/prometheus.guard';
 
 @Controller()
 @ApiExcludeController()
-export class PrometheusMetricController extends PrometheusController {
-  constructor(private readonly prismaService: PrismaService) {
-    super();
-  }
-
-  @Get()
-  @UseGuards(PrometheusAuthGuard)
-  public async index(@Res({ passthrough: true }) response: Response) {
-    return super.index(response);
-  }
+export class PrometheusMetricController {
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get('db-metrics')
   @UseGuards(PrometheusAuthGuard)
--- src/prometheus/prometheus.module.ts
+++ src/prometheus/prometheus.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrometheusModule as PrometheusModuleSource } from '@willsoto/nestjs-prometheus';
 import { PostgresService } from 'src/db/postgres.service';
+import { PrismaService } from 'src/db/prisma.service';
 import { AggregateMetrics } from './aggregate-metrics';
 import { aggregatePrometheusMetrics } from './aggregate-metrics/metrics';
 import { AllocatorReportGeneratorMetrics } from './allocator-metrics';
@@ -9,15 +11,15 @@ import { ClientReportGeneratorMetrics } from './client-metrics';
 import { clientPrometheusMetrics } from './client-metrics/metrics';
 import { PgPoolMetrics } from './db-metrics';
 import { pgPoolPrometheusMetrics } from './db-metrics/metrics';
+import { PrometheusCustomMetricController } from './prometheus-custom.controller';
 import { PrometheusMetricController } from './prometheus.controller';
 import { PrometheusMetricService } from './prometheus.service';
-import { ConfigService } from '@nestjs/config';
-import { PrismaService } from 'src/db/prisma.service';
 
 @Module({
   imports: [
     PrometheusModuleSource.register({
       customMetricPrefix: 'cdp',
+      controller: PrometheusCustomMetricController,
       defaultMetrics: {
         enabled: false,
       },
