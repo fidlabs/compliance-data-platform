daa020bac707760fd540d2322e2ee190fb166136
lukas <lukasz.wal@neti-soft.com>
2025-03-04 10:46:16 +0100

    Update pg pool stats instant instead of CRON

--- src/db/postgres.service.ts
+++ src/db/postgres.service.ts
@@ -1,30 +1,40 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { Pool } from 'pg';
 import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+import { PrometheusMetricService } from 'src/prometheus';
 
 @Injectable()
 export class PostgresService implements OnModuleInit {
   public pool: Pool;
 
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    public readonly prometheusService: PrometheusMetricService,
+  ) {}
+
+  updatePoolStats() {
+    const { totalCount, idleCount, waitingCount } = this.pool;
+
+    this.prometheusService.updatePgPoolMetrics(
+      totalCount,
+      idleCount,
+      waitingCount,
+    );
+  }
 
   public async onModuleInit() {
     this.pool = new Pool({
       connectionString: this.configService.get<string>('DATABASE_URL'),
     });
-  }
 
-  public async getMetrics(): Promise<{
-    totalCount: number;
-    idleCount: number;
-    waitingCount: number;
-  }> {
-    const { totalCount, idleCount, waitingCount } = this.pool;
-
-    return {
-      totalCount,
-      idleCount,
-      waitingCount,
-    };
+    this.pool.on('connect', () => {
+      this.updatePoolStats();
+    });
+    this.pool.on('acquire', () => {
+      this.updatePoolStats();
+    });
+    this.pool.on('remove', () => {
+      this.updatePoolStats();
+    });
   }
 }
--- src/prometheus/prometheus.service.ts
+++ src/prometheus/prometheus.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
-import { PostgresService } from 'src/db/postgres.service';
 import { AggregateMetrics } from './aggregate-metrics';
 import { AllocatorReportGeneratorMetrics } from './allocator-metrics';
 import { ClientReportGeneratorMetrics } from './client-metrics';
@@ -9,19 +7,18 @@ import { PgPoolMetrics } from './db-metrics';
 @Injectable()
 export class PrometheusMetricService {
   constructor(
-    private readonly postgresService: PostgresService,
     public readonly clientReportGeneratorMetrics: ClientReportGeneratorMetrics,
     public readonly allocatorReportGeneratorMetrics: AllocatorReportGeneratorMetrics,
     public readonly pgPoolMetrics: PgPoolMetrics,
     public readonly aggregateMetrics: AggregateMetrics,
   ) {}
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
-  async updateMetrics() {
+  updatePgPoolMetrics(
+    totalCount: number,
+    idleCount: number,
+    waitingCount: number,
+  ) {
     try {
-      const { idleCount, totalCount, waitingCount } =
-        await this.postgresService.getMetrics();
-
       this.pgPoolMetrics.setPgPoolExistClientCount(totalCount);
       this.pgPoolMetrics.setPgPoolIdleClientCount(idleCount);
       this.pgPoolMetrics.setPgPoolWaitingClientCount(waitingCount);
