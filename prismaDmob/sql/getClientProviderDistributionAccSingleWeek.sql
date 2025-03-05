d43c4aca7db74848a521a2bf6470f2068561de0d
Kacper Żuk <kacper.zuk@neti-soft.com>
2025-03-05 15:10:46 +0100

    Fetch client-provider-distribution-acc week-by-week

---
+++ prismaDmob/sql/getClientProviderDistributionAccSingleWeek.sql
@@ -0,0 +1,26 @@
+with miner_pieces as (
+    select
+        "pieceCid" as piece_cid,
+        'f0' || "clientId" as client,
+        'f0' || "providerId" as provider,
+        sum("pieceSize") as total_deal_size,
+        min("pieceSize") as piece_size
+    from unified_verified_deal
+    where
+        "termStart" >= 3847920 -- nv22 start
+        and date_trunc('week', to_timestamp("termStart" * 30 + 1598306400)) <= $1 -- deals up to provided week
+    group by
+      "pieceCid",
+      "clientId",
+      "providerId"
+)
+
+select
+    client,
+    provider,
+    sum(total_deal_size)::bigint as total_deal_size,
+    sum(piece_size)::bigint as unique_data_size
+from miner_pieces
+group by client, provider;
+
+
--- src/aggregation/runners/client-provider-distribution-acc.runner.ts
+++ src/aggregation/runners/client-provider-distribution-acc.runner.ts
@@ -1,4 +1,6 @@
-import { QueryIterablePool } from 'pg-iterator';
+import { Logger } from '@nestjs/common';
+import { DateTime } from 'luxon';
+import { getClientProviderDistributionAccSingleWeek } from 'prismaDmob/generated/client/sql';
 import {
   AggregationRunner,
   AggregationRunnerRunServices,
@@ -6,117 +8,72 @@ import {
 import { AggregationTable } from '../aggregation-table';
 
 export class ClientProviderDistributionAccRunner implements AggregationRunner {
+  private readonly logger = new Logger(
+    ClientProviderDistributionAccRunner.name,
+  );
+
   public async run({
     prismaService,
-    postgresDmobService,
+    prismaDmobService,
     prometheusMetricService,
   }: AggregationRunnerRunServices): Promise<void> {
     const runnerName = this.getName();
-
     const {
       startGetDataTimerByRunnerNameMetric,
       startStoreDataTimerByRunnerNameMetric,
     } = prometheusMetricService.aggregateMetrics;
 
-    await prismaService.$transaction(
-      async (tx) => {
-        const getDataEndTimerMetric =
-          startGetDataTimerByRunnerNameMetric(runnerName);
+    //const latestStored =
+    //  await prismaService.client_provider_distribution_weekly_acc.findFirst({
+    //    select: {
+    //      week: true,
+    //    },
+    //    orderBy: {
+    //      week: 'desc',
+    //    },
+    //  });
+    //let nextWeek = latestStored
+    //  ? DateTime.fromJSDate(latestStored.date, { zone: 'UTC' }) // we want to reprocess the last stored week, as it might've been incomplete
+    //  : DateTime.fromSeconds(3847920 * 30 + 1598306400).startOf('week'); // nv22 start week - a.k.a. reprocess everything
+    let nextWeek = DateTime.fromSeconds(3847920 * 30 + 1598306400).startOf(
+      'week',
+    ); // nv22 start week
 
-        const queryIterablePool = new QueryIterablePool<{
-          week: Date | null;
-          client: string | null;
-          provider: string | null;
-          total_deal_size: bigint | null;
-          unique_data_size: bigint | null;
-        }>(postgresDmobService.pool);
+    const now = DateTime.now().setZone('UTC');
+    while (nextWeek <= now) {
+      this.logger.debug(`Processing week ${nextWeek}`);
+      const getDataEndTimerMetric =
+        startGetDataTimerByRunnerNameMetric(runnerName);
+      const result = await prismaDmobService.$queryRawTyped(
+        getClientProviderDistributionAccSingleWeek(nextWeek.toJSDate()),
+      );
+      getDataEndTimerMetric();
 
-        const i = queryIterablePool.query(`with miner_pieces
-                                                  as (select date_trunc('week', to_timestamp("termStart" * 30 + 1598306400)) as week,
-                                                             'f0' || "clientId"                                              as client,
-                                                             'f0' || "providerId"                                            as provider,
-                                                             "pieceCid",
-                                                             sum("pieceSize")                                                as total_deal_size,
-                                                             min("pieceSize")                                                as piece_size
-                                                      from unified_verified_deal
-                                                      where "termStart" >= 3847920                                           -- nv22 start
-                                                        and to_timestamp("termStart" * 30 + 1598306400) <= current_timestamp -- deals that didn't start yet
-                                                      group by week,
-                                                               client,
-                                                               provider,
-                                                               "pieceCid"),
-                                              weeks as (select date_trunc('week', dates) week
-                                                        from generate_series(
-                                                                     to_timestamp(3847920 * 30 + 1598306400),
-                                                                     current_timestamp,
-                                                                     '1 week'::interval) dates)
-                                         select weeks.week                   as week,
-                                                client,
-                                                provider,
-                                                sum(total_deal_size)::bigint as total_deal_size,
-                                                sum(piece_size)::bigint      as unique_data_size
-                                         from weeks
-                                                  inner join miner_pieces
-                                                             on weeks.week >= miner_pieces.week
-                                         group by weeks.week,
-                                                  client,
-                                                  provider;`);
+      const storeDataEndTimerMetric =
+        startStoreDataTimerByRunnerNameMetric(runnerName);
+      const data = result.map((dmobResult) => ({
+        week: nextWeek.toJSDate(),
+        client: dmobResult.client,
+        provider: dmobResult.provider,
+        total_deal_size: dmobResult.total_deal_size,
+        unique_data_size: dmobResult.unique_data_size,
+      }));
+      await prismaService.$transaction(async (tx) => {
+        await tx.client_provider_distribution_weekly_acc.deleteMany({
+          where: {
+            week: {
+              equals: nextWeek.toJSDate(),
+            },
+          },
+        });
+        await tx.client_provider_distribution_weekly_acc.createMany({
+          data,
+        });
+      });
+      storeDataEndTimerMetric();
 
-        const data: {
-          week: Date | null;
-          client: string | null;
-          provider: string | null;
-          total_deal_size: bigint | null;
-          unique_data_size: bigint | null;
-        }[] = [];
-
-        let isFirstInsert = true;
-
-        let storeDataEndTimerMetric;
-
-        for await (const rowResult of i) {
-          data.push({
-            week: rowResult.week,
-            client: rowResult.client,
-            provider: rowResult.provider,
-            total_deal_size: rowResult.total_deal_size,
-            unique_data_size: rowResult.unique_data_size,
-          });
-
-          if (data.length === 5000) {
-            if (isFirstInsert) {
-              getDataEndTimerMetric();
-              storeDataEndTimerMetric =
-                startStoreDataTimerByRunnerNameMetric(runnerName);
-              await tx.$executeRaw`delete from client_provider_distribution_weekly_acc`;
-              isFirstInsert = false;
-            }
-
-            await tx.client_provider_distribution_weekly_acc.createMany({
-              data,
-            });
-
-            data.length = 0;
-          }
-        }
-
-        if (data.length > 0) {
-          if (isFirstInsert) {
-            getDataEndTimerMetric();
-            storeDataEndTimerMetric =
-              startStoreDataTimerByRunnerNameMetric(runnerName);
-            await tx.$executeRaw`delete from client_provider_distribution_weekly_acc`;
-          }
-          await tx.client_provider_distribution_weekly_acc.createMany({
-            data,
-          });
-        }
-        storeDataEndTimerMetric();
-      },
-      {
-        timeout: Number.MAX_SAFE_INTEGER,
-      },
-    );
+      nextWeek = nextWeek.plus({ weeks: 1 });
+    }
   }
 
   getFilledTables(): AggregationTable[] {
