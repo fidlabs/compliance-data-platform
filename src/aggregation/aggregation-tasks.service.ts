36e6e184f00eca01caa9fd320a9b187d39e9b076
pingwindyktator <pingwindyktator@pm.me>
2025-03-06 17:53:28 +0100

    Run db aggregations every 2 hours

--- src/aggregation/aggregation-tasks.service.ts
+++ src/aggregation/aggregation-tasks.service.ts
@@ -33,7 +33,7 @@ export class AggregationTasksService extends HealthIndicator {
     throw new HealthCheckError('Healthcheck failed', result);
   }
 
-  @Cron(CronExpression.EVERY_HOUR)
+  @Cron(CronExpression.EVERY_2_HOURS)
   public async runAggregationJob() {
     if (!this.jobInProgress) {
       this.jobInProgress = true;
