63cacac569f69972d873bff4d4dfe3a464e711ab
pingwindyktator <pingwindyktator@pm.me>
2025-03-19 16:51:23 +0100

    Reformat .sql files

--- prismaDmob/sql/getOldDatacapBalanceNv22.sql
+++ prismaDmob/sql/getOldDatacapBalanceNv22.sql
@@ -1,25 +1,17 @@
-with
-old_dc_allowance as (
-    select
-        "addressId" as allocator,
-        sum(allowance) as total_allowance
-    from verifier_allowance
-    where height < 3847920 -- nv22 start
-    group by "addressId"
-),
+with "old_dc_allowance" as (select "addressId"      as "allocator",
+                                   sum("allowance") as "total_allowance"
+                            from "verifier_allowance"
+                            where "height" < 3847920 -- nv22 start
+                            group by "addressId"),
 
-old_dc_allocations as (
-    select
-        "verifierAddressId" as allocator,
-        sum(allowance) as total_allowance
-    from verified_client_allowance
-    where height < 3847920 -- nv22 start
-    group by "verifierAddressId"
-)
+     "old_dc_allocations" as (select "verifierAddressId" as "allocator",
+                                     sum("allowance")    as "total_allowance"
+                              from "verified_client_allowance"
+                              where "height" < 3847920 -- nv22 start
+                              group by "verifierAddressId")
 
-select
-    dc_in.allocator,
-    greatest(0, dc_in.total_allowance - dc_out.total_allowance)::bigint as old_dc_balance
-from old_dc_allowance as dc_in
-inner join old_dc_allocations as dc_out
-    using (allocator);
+select "dc_in"."allocator",
+       greatest(0, "dc_in"."total_allowance" - "dc_out"."total_allowance")::bigint as "old_dc_balance"
+from "old_dc_allowance" as "dc_in"
+         inner join "old_dc_allocations" as "dc_out"
+                    using ("allocator");
