c119a50bf6860ce3a585ed1ae1ae77f258fdc6eb
pingwindyktator <pingwindyktator@pm.me>
2025-03-20 16:23:14 +0100

    Fix LEFT JOIN filtering on allocator table

--- prisma/sql/getWeekAverageStandardAllocatorRetrievability.sql
+++ prisma/sql/getWeekAverageStandardAllocatorRetrievability.sql
@@ -1,5 +1,5 @@
 select avg("avg_weighted_retrievability_success_rate") as "average"
 from "allocators_weekly"
          left join "allocator" on "allocators_weekly"."allocator" = "allocator"."id"
-where "allocator"."is_metaallocator" != true
+where "allocator"."is_metaallocator" = false or "allocator"."is_metaallocator" is null
   and "week" = $1;
