30e84db6b559c4809d2646f8f611f5fa9ac6a3c5
pingwindyktator <pingwindyktator@pm.me>
2025-05-07 19:24:14 +0200

    Reformat .sql files

--- prisma/sql/getIpniReportingWeekly.sql
+++ prisma/sql/getIpniReportingWeekly.sql
@@ -1,19 +1,15 @@
-with with_week as (
-    select
-        date,
-        ok,
-        misreporting,
-        not_reporting,
-        total,
-        date_trunc('week', date) as "week"
-    from ipni_reporting_daily
-)
+with "with_week" as (select "date",
+                            "ok",
+                            "misreporting",
+                            "not_reporting",
+                            "total",
+                            date_trunc('week', "date") as "week"
+                     from "ipni_reporting_daily")
 
-select distinct on (week)
-    week,
-    ok,
-    not_reporting,
-    misreporting,
-    total
-from with_week
-order by week, date desc;
+select distinct on ("week") "week",
+                            "ok",
+                            "not_reporting",
+                            "misreporting",
+                            "total"
+from "with_week"
+order by "week", "date" desc;
--- prisma/sql/getProvidersWithIpInfo.sql
+++ prisma/sql/getProvidersWithIpInfo.sql
@@ -1,15 +1,12 @@
-with
+with "latest_updates" as (select "provider", max("date") as "date"
+                          from "provider_ip_info"
+                          group by "provider")
 
-latest_updates as (
-  select provider, max(date) as date from provider_ip_info group by provider
-)
-
-select
-    provider,
-    provider_ip_info.lat,
-    provider_ip_info.long,
-    provider_ip_info.country,
-    provider_ip_info.region,
-    provider_ip_info.city
-from provider_ip_info
-inner join latest_updates using (provider, date);
+select "provider",
+       "provider_ip_info"."lat",
+       "provider_ip_info"."long",
+       "provider_ip_info"."country",
+       "provider_ip_info"."region",
+       "provider_ip_info"."city"
+from "provider_ip_info"
+         inner join "latest_updates" using ("provider", "date");
