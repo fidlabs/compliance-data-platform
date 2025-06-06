6109c5f86c9a5ac79c4a228ba0a2a14eaca7d949
pingwindyktator <pingwindyktator@pm.me>
2025-06-06 12:33:00 +0200

    Sort /report-checks results, fix report generation bug

--- src/service/client-report/client-report.service.ts
+++ src/service/client-report/client-report.service.ts
@@ -76,7 +76,7 @@ export class ClientReportService {
         is_public_dataset: bookkeepingInfo?.isPublicDataset,
         using_client_contract: !!bookkeepingInfo?.clientContractAddress,
         storage_provider_ids_declared:
-          bookkeepingInfo.storageProviderIDsDeclared,
+          bookkeepingInfo?.storageProviderIDsDeclared,
         client_contract_max_deviation: maxDeviation,
         storage_provider_distribution: {
           create:
@@ -84,7 +84,7 @@ export class ClientReportService {
               return {
                 ...provider,
                 declared_in_application_file:
-                  bookkeepingInfo.storageProviderIDsDeclared.includes(
+                  bookkeepingInfo?.storageProviderIDsDeclared?.includes(
                     provider.provider,
                   ),
                 ...(provider.location && {
