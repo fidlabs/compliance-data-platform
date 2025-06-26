b5370e70b31945550822c673a9f712f910a39b51
pingwindyktator <pingwindyktator@pm.me>
2025-06-26 12:58:31 +0200

    Show changes in the number of report checks failed / passed

--- prisma/sql/getAllocatorReportChecksDetails.sql
+++ prisma/sql/getAllocatorReportChecksDetails.sql
@@ -1,19 +1,37 @@
 -- @param {DateTime} $1:day
 
-select (count(distinct "check") filter ( where "result" = true))::int  as "checksPassedCount",
-       (count(distinct "check") filter ( where "result" = false))::int as "checksFailedCount",
-       "allocator"                                                     as "allocatorId",
-       "name"                                                          as "allocatorName",
-       coalesce(
-                       jsonb_agg(
-                       distinct jsonb_build_object(
-                               'reportId', "allocator_report_id",
-                               'check', "check",
-                               'checkMsg', "metadata"::jsonb->'msg'
-                                )
-                                ) filter ( where "result" = false ), '[]'::jsonb
-       )                                                        as "failedChecks"
-from "allocator_report_check_result"
-         join "allocator_report" on "allocator_report_check_result"."allocator_report_id" = "allocator_report"."id"
-where date_trunc('day', "allocator_report_check_result"."create_date") = $1
-group by "allocatorId", "allocatorName";
+with "today" as (select (count(distinct "check") filter ( where "result" = true))::int  as "checksPassedCount",
+                        (count(distinct "check") filter ( where "result" = false))::int as "checksFailedCount",
+                        "allocator"                                                     as "allocatorId",
+                        "name"                                                          as "allocatorName",
+                        coalesce(
+                                        jsonb_agg(
+                                        distinct jsonb_build_object(
+                                                'reportId', "allocator_report_id",
+                                                'check', "check",
+                                                'checkMsg', "metadata"::jsonb -> 'msg'
+                                                 )
+                                                 ) filter ( where "result" = false ), '[]'::jsonb
+                        )                                                               as "failedChecks"
+                 from "allocator_report_check_result"
+                          join "allocator_report" on "allocator_report_check_result"."allocator_report_id" = "allocator_report"."id"
+                 where date_trunc('day', "allocator_report_check_result"."create_date") = $1
+                 group by "allocatorId", "allocatorName"),
+     "yesterday" as (select (count(distinct "check") filter ( where "result" = true))::int  as "checksPassedCount",
+                            (count(distinct "check") filter ( where "result" = false))::int as "checksFailedCount",
+                            "allocator"                                                     as "allocatorId"
+                     from "allocator_report_check_result"
+                              join "allocator_report" on "allocator_report_check_result"."allocator_report_id" = "allocator_report"."id"
+                     where date_trunc('day', "allocator_report_check_result"."create_date") = date_trunc('day', $1::timestamp) - interval '1 day'
+                     group by "allocatorId")
+select "today".*,
+       case
+           when "yesterday"."checksPassedCount" is not null then
+               ("today"."checksPassedCount" - "yesterday"."checksPassedCount")
+           end as "checksPassedChange",
+       case
+           when "yesterday"."checksFailedCount" is not null then
+               ("today"."checksFailedCount" - "yesterday"."checksFailedCount")
+           end as "checksFailedChange"
+from "today"
+         left join "yesterday" on "today"."allocatorId" = "yesterday"."allocatorId";
--- prisma/sql/getAllocatorReportChecksWeekly.sql
+++ prisma/sql/getAllocatorReportChecksWeekly.sql
@@ -1,12 +1,29 @@
-with "checks_weekly" as (select count(distinct "check") filter ( where "result" = true)  as "checksPassedCount",
-                                count(distinct "check") filter ( where "result" = false) as "checksFailedCount",
-                                date_trunc('week', "allocator_report"."create_date")     as "week"
-                         from "allocator_report_check_result"
-                                  join "allocator_report" on "allocator_report_check_result"."allocator_report_id" = "allocator_report"."id"
-                         group by "allocator", "week")
-select sum("checksPassedCount")::int as "checksPassedCount",
-       sum("checksFailedCount")::int as "checksFailedCount",
-       "week"                        as "week"
-from "checks_weekly"
-group by "week"
+with "checks_weekly_per_allocator" as (select count(distinct "check") filter ( where "result" = true)  as "checksPassedCount",
+                                              count(distinct "check") filter ( where "result" = false) as "checksFailedCount",
+                                              date_trunc('week', "allocator_report"."create_date")     as "week"
+                                       from "allocator_report_check_result"
+                                                join "allocator_report" on "allocator_report_check_result"."allocator_report_id" = "allocator_report"."id"
+                                       group by "allocator", "week"),
+     "checks_weekly" as (select sum("checksPassedCount")::int as "checksPassedCount",
+                                sum("checksFailedCount")::int as "checksFailedCount",
+                                "week"                        as "week"
+                         from "checks_weekly_per_allocator"
+                         group by "week"),
+     "checks_weekly_with_lag" as (select *,
+                                         lag("checksPassedCount") over (order by "week") as "checksPassedCountLag",
+                                         lag("checksFailedCount") over (order by "week") as "checksFailedCountLag",
+                                         lag("week") over (order by "week")              as "weekLag"
+                                  from "checks_weekly")
+select "week"                 as "week",
+       "checksPassedCount"    as "checksPassedCount",
+       case
+           when "weekLag" is not null and "week" - "weekLag" = interval '7 days'
+               then "checksPassedCount" - "checksPassedCountLag"
+           end                as "checksPassedChange",
+       "checksFailedCount"    as "checksFailedCount",
+       case
+           when "weekLag" is not null and "week" - "weekLag" = interval '7 days'
+               then "checksFailedCount" - "checksFailedCountLag"
+           end                as "checksFailedChange"
+from "checks_weekly_with_lag"
 order by "week" desc;
