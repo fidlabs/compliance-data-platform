98d0550caea0cb4e74652a096cd0f8e6a731f9da
pingwindyktator <pingwindyktator@pm.me>
2025-06-27 17:39:36 +0200

    Report checks: mark checks as new

--- src/controller/report-checks/types.report-checks.ts
+++ src/controller/report-checks/types.report-checks.ts
@@ -59,6 +59,29 @@ export class ReportCheck {
     enum: AllocatorReportCheck,
   })
   check: AllocatorReportCheck;
+
+  @ApiProperty({
+    type: String,
+    format: 'date',
+    example: '2024-04-22T00:00:00.000Z',
+    description: 'Time when the check was first seen; ISO format',
+  })
+  firstSeen: Date;
+
+  @ApiProperty({
+    type: String,
+    format: 'date',
+    example: '2024-04-22T00:00:00.000Z',
+    description:
+      'Time when the check was last seen before requested day; null if not seen before; ISO format',
+  })
+  lastSeen: Date | null;
+
+  @ApiProperty({
+    description:
+      'Whether the check is new since a week ago (i.e. not seen in last week)',
+  })
+  isNew: boolean;
 }
 
 export class AllocatorReportChecksDetails extends ReportChecksCount {
