6699de03e4a64cb57ca95444d28fb853ad340188
pingwindyktator <pingwindyktator@pm.me>
2025-08-21 17:12:02 +0200

    Add FilPlus edition 5 data for allocator audits endpoints

---
+++ src/controller/base/filplus-edition-controller-base.ts
@@ -0,0 +1,33 @@
+import {
+  FilPlusEdition,
+  getCurrentFilPlusEdition,
+  getFilPlusEditionById,
+} from 'src/utils/filplus-edition';
+import { FilPlusEditionRequest } from './types.filplus-edition-controller-base';
+import { stringToNumber } from 'src/utils/utils';
+import { BadRequestException } from '@nestjs/common';
+import { ControllerBase } from './controller-base';
+
+export class FilPlusEditionControllerBase extends ControllerBase {
+  public getFilPlusEditionFromRequest(
+    query: FilPlusEditionRequest,
+  ): FilPlusEdition {
+    if (query?.editionId) {
+      try {
+        const edition = getFilPlusEditionById(stringToNumber(query.editionId));
+
+        if (edition) {
+          return edition;
+        } else {
+          // noinspection ExceptionCaughtLocallyJS
+          throw new Error('Edition not found');
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (_) {
+        throw new BadRequestException(`Invalid editionId: ${query.editionId}`);
+      }
+    } else {
+      return getCurrentFilPlusEdition();
+    }
+  }
+}
---
+++ src/service/allocator/resources/README.md
@@ -0,0 +1,2 @@
+The data from this directory comes from the old 'Allocator Org Overview' Google Sheet document which is now deprecated and can't be used.
+The data was scraped from there and now acts as a source of truth for FilPlus edition 5 allocator audits data.
