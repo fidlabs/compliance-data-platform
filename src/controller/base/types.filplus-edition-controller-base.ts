75335b2bb87b19a0f42e55621a2115d4326dbeb9
pingwindyktator <pingwindyktator@pm.me>
2025-08-25 11:45:44 +0200

    Default to all editions combined in allocator audits

--- src/controller/base/types.filplus-edition-controller-base.ts
+++ src/controller/base/types.filplus-edition-controller-base.ts
@@ -3,6 +3,15 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 export class FilPlusEditionRequest {
   @ApiPropertyOptional({
     description:
+      'FilPlus edition ID to filter by; default is all editions combined',
+    example: '6',
+  })
+  editionId?: string;
+}
+
+export class FilPlusEditionDefaultCurrentRequest {
+  @ApiPropertyOptional({
+    description:
       'FilPlus edition ID to filter by; default is the current edition',
     example: '6',
   })
