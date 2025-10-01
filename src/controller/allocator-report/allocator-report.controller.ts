8a6be3e565163f694884600a0052bc7f8971415a
pingwindyktator <pingwindyktator@pm.me>
2025-10-01 15:58:45 +0200

    Review fixes

--- src/controller/allocator-report/allocator-report.controller.ts
+++ src/controller/allocator-report/allocator-report.controller.ts
@@ -19,6 +19,7 @@ import {
 import { AllocatorReportService } from 'src/service/allocator-report/allocator-report.service';
 import { GetAllocatorReportRequest } from '../allocators/types.allocators';
 import { ControllerBase } from '../base/controller-base';
+import { PaginationInfoRequest } from '../base/types.controller-base';
 
 @Controller('allocator-report')
 export class AllocatorReportController extends ControllerBase {
@@ -59,45 +60,19 @@ export class AllocatorReportController extends ControllerBase {
       limit: query.clientPaginationLimit,
       page: query.clientPaginationPage,
     };
+
     const providerPagination = {
       limit: query.providerPaginationLimit,
       page: query.providerPaginationPage,
     };
 
-    const paginationInfoClient = this.validatePaginationInfo(clientPagination);
-    const paginationInfoProvider =
-      this.validatePaginationInfo(providerPagination);
-
     const report = await this.allocatorReportService.getLatestReport(
       allocator,
-      paginationInfoClient,
-      paginationInfoProvider,
+      this.validatePaginationInfo(clientPagination),
+      this.validatePaginationInfo(providerPagination),
     );
 
-    if (!report) throw new NotFoundException();
-
-    return {
-      ...report,
-      clients: this.withPaginationInfo(
-        {
-          count: report.clients?.length,
-          data: this.paginated(this.sorted(report.clients), clientPagination),
-        },
-        clientPagination,
-        report.clients_total,
-      ),
-      storage_provider_distribution: this.withPaginationInfo(
-        {
-          count: report.storage_provider_distribution?.length,
-          data: this.paginated(
-            this.sorted(report.storage_provider_distribution),
-            providerPagination,
-          ),
-        },
-        providerPagination,
-        report.storage_provider_distribution_total,
-      ),
-    };
+    return this.paginatedReport(report, clientPagination, providerPagination);
   }
 
   @Get(':allocator/:id')
@@ -119,38 +94,39 @@ export class AllocatorReportController extends ControllerBase {
     id: string,
     @Query() query: GetAllocatorReportRequest,
   ) {
-    query.clientPaginationLimit ??= '100';
-    query.clientPaginationPage ??= '1';
-    query.providerPaginationLimit ??= '100';
-    query.providerPaginationPage ??= '1';
-
     const clientPagination = {
       limit: query.clientPaginationLimit,
       page: query.clientPaginationPage,
     };
+
     const providerPagination = {
       limit: query.providerPaginationLimit,
       page: query.providerPaginationPage,
     };
 
-    const paginationInfoClient = this.validatePaginationInfo(clientPagination);
-    const paginationInfoProvider =
-      this.validatePaginationInfo(providerPagination);
-
     const report = await this.allocatorReportService.getReport(
       allocator,
       id,
-      paginationInfoClient,
-      paginationInfoProvider,
+      this.validatePaginationInfo(clientPagination),
+      this.validatePaginationInfo(providerPagination),
     );
 
+    return this.paginatedReport(report, clientPagination, providerPagination);
+  }
+
+  private async paginatedReport(
+    report,
+    clientPagination: PaginationInfoRequest,
+    providerPagination: PaginationInfoRequest,
+  ) {
     if (!report) throw new NotFoundException();
+
     return {
       ...report,
       clients: this.withPaginationInfo(
         {
           count: report.clients?.length,
-          data: this.paginated(this.sorted(report.clients), clientPagination),
+          data: report.clients,
         },
         clientPagination,
         report.clients_total,
@@ -158,10 +134,7 @@ export class AllocatorReportController extends ControllerBase {
       storage_provider_distribution: this.withPaginationInfo(
         {
           count: report.storage_provider_distribution?.length,
-          data: this.paginated(
-            this.sorted(report.storage_provider_distribution),
-            providerPagination,
-          ),
+          data: report.storage_provider_distribution,
         },
         providerPagination,
         report.storage_provider_distribution_total,
