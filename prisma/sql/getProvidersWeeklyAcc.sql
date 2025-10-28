86e7b0785093f0d7d7d08666728804e879524f5c
pingwindyktator <pingwindyktator@pm.me>
2025-10-28 17:34:01 +0100

    Refactor sqls

--- prisma/sql/getProvidersWeeklyAcc.sql
+++ prisma/sql/getProvidersWeeklyAcc.sql
@@ -1,46 +1,36 @@
-WITH provider_retrievability_weekly AS (
-    SELECT
-        prd."week",
-        prd."provider",
-        SUM(prd."total") AS total,
-        SUM(prd."successful") AS successful,
-        SUM(prd."successful"::float) / SUM(prd."total"::float) AS success_rate,
-        SUM(COALESCE(prd."successful_http", 0)) AS successful_http,
-        SUM(COALESCE(prd."successful_http", 0)::float) / SUM(prd."total"::float) AS success_rate_http,
-        COALESCE(
-            (
-                SELECT puf."success_rate"
-                FROM "provider_url_finder_retrievability_daily" puf
-                WHERE puf."provider" = prd."provider"
-                  AND DATE_TRUNC('week', puf."date") = prd."week"
-                ORDER BY puf."date" DESC
-                LIMIT 1
-            ), 0
-        ) AS success_rate_url_finder
-    FROM (
-        SELECT
-            DATE_TRUNC('week', "date") AS "week",
-            "provider",
-            "total",
-            "successful",
-            "successful_http"
-        FROM "provider_retrievability_daily"
-    ) AS prd
-    GROUP BY prd."week", prd."provider"
-)
-SELECT 
-    COALESCE(prw."week", DATE_TRUNC('week', cpw."week")) AS week,
-    cpw."provider",
-    COUNT(*)::int AS num_of_clients,
-    MAX(cpw."total_deal_size")::bigint AS biggest_client_total_deal_size,
-    SUM(cpw."total_deal_size")::bigint AS total_deal_size,
-    MAX(COALESCE(prw."success_rate", 0)) AS avg_retrievability_success_rate,
-    MAX(COALESCE(prw."success_rate_http", 0)) AS avg_retrievability_success_rate_http,
-    MAX(COALESCE(prw."success_rate_url_finder", 0)) AS avg_retrievability_success_rate_url_finder
-FROM "client_provider_distribution_weekly_acc" cpw
-LEFT JOIN provider_retrievability_weekly prw
-    ON DATE_TRUNC('week', cpw."week") = prw."week"
-   AND cpw."provider" = prw."provider"
-GROUP BY
-    COALESCE(prw."week", DATE_TRUNC('week', cpw."week")),
-    cpw."provider";
+with "provider_retrievability_weekly" as (select "prd"."week"                                                                 as "week",
+                                                 "prd"."provider"                                                             as "provider",
+                                                 sum("prd"."total")                                                           as "total",
+                                                 sum("prd"."successful")                                                      as "successful",
+                                                 sum("prd"."successful"::float) / sum("prd"."total"::float)                   as "success_rate",
+                                                 sum(coalesce("prd"."successful_http", 0))                                    as "successful_http",
+                                                 sum(coalesce("prd"."successful_http", 0)::float) / sum("prd"."total"::float) as "success_rate_http",
+                                                 coalesce(
+                                                         (select "puf"."success_rate"
+                                                          from "provider_url_finder_retrievability_daily" "puf"
+                                                          where "puf"."provider" = "prd"."provider"
+                                                            and date_trunc('week', "puf"."date") = "prd"."week"
+                                                          order by "puf"."date" desc
+                                                          limit 1), 0
+                                                 )                                                                            as "success_rate_url_finder"
+--
+                                          from (select date_trunc('week', "date") as "week",
+                                                       "provider"                 as "provider",
+                                                       "total"                    as "total",
+                                                       "successful"               as "successful",
+                                                       "successful_http"          as "successful_http"
+                                                from "provider_retrievability_daily") as "prd"
+                                          group by "prd"."week", "prd"."provider")
+--
+select coalesce("prw"."week", date_trunc('week', "cpw"."week")) as "week",
+       "cpw"."provider"                                         as "provider",
+       count(*)::int                                            as "num_of_clients",
+       max("cpw"."total_deal_size")::bigint                     as "biggest_client_total_deal_size",
+       sum("cpw"."total_deal_size")::bigint                     as "total_deal_size",
+       max(coalesce("prw"."success_rate", 0))                   as "avg_retrievability_success_rate",
+       max(coalesce("prw"."success_rate_http", 0))              as "avg_retrievability_success_rate_http",
+       max(coalesce("prw"."success_rate_url_finder", 0))        as "avg_retrievability_success_rate_url_finder"
+from "client_provider_distribution_weekly_acc" "cpw"
+         left join "provider_retrievability_weekly" "prw"
+                   on date_trunc('week', "cpw"."week") = "prw"."week" and "cpw"."provider" = "prw"."provider"
+group by coalesce("prw"."week", date_trunc('week', "cpw"."week")), "cpw"."provider";
