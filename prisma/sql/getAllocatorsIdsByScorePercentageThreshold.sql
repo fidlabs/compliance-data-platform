f72ca01d88e90a15488a5d1425fa132e05f81d99
pingwindyktator <pingwindyktator@pm.me>
2025-12-10 16:52:53 +0100

    Reformat sqls

--- prisma/sql/getAllocatorsIdsByScorePercentageThreshold.sql
+++ prisma/sql/getAllocatorsIdsByScorePercentageThreshold.sql
@@ -1,25 +1,25 @@
 -- @param {Decimal} $1:minScorePercentage
 -- @param {Decimal} $2:maxScorePercentage
 -- @param {DateTime} $3:thresholdDate
- WITH "max_range" AS
-  (SELECT "scoring_result_id",
-          MAX("score") AS "max_score"
-   FROM "allocator_report_scoring_result_range"
-   GROUP BY "scoring_result_id"),
-      "allocator_score" AS
-  (SELECT "report"."allocator",
-          SUM("scoring_result"."score")::DECIMAL / NULLIF(SUM("range"."max_score"), 0)::DECIMAL AS "score_percentage",
-          ROW_NUMBER() OVER (PARTITION BY "report"."allocator"
-                             ORDER BY "report"."create_date" DESC) AS "row_index"
-   FROM "allocator_report" AS "report"
-   LEFT JOIN "allocator_report_scoring_result" AS "scoring_result" ON "report"."id" = "scoring_result"."allocator_report_id"
-   LEFT JOIN "max_range" AS range ON "range"."scoring_result_id" = "scoring_result"."id"
-   WHERE "report"."create_date" <= $3::DATE
-   GROUP BY "report"."allocator",
-            "report"."create_date"
-   ORDER BY "report"."allocator",
-            "report"."create_date" DESC)
-SELECT "allocator"
-FROM "allocator_score"
-WHERE "row_index" = 1
-  AND "score_percentage" BETWEEN $1::DECIMAL AND $2::DECIMAL;
+
+with "max_range" as (select "scoring_result_id" as "scoring_result_id",
+                            max("score")        as "max_score"
+                     from "allocator_report_scoring_result_range"
+                     group by "scoring_result_id"),
+--
+     "allocator_score" as (select "report"."allocator"                                                                       as "allocator",
+                                  sum("scoring_result"."score")::decimal / nullif(sum("range"."max_score"), 0)::decimal      as "score_percentage",
+                                  row_number() over (partition by "report"."allocator" order by "report"."create_date" desc) as "row_index"
+                           from "allocator_report" as "report"
+                                    left join "allocator_report_scoring_result" as "scoring_result"
+                                              on "report"."id" = "scoring_result"."allocator_report_id"
+                                    left join "max_range" as "range"
+                                              on "range"."scoring_result_id" = "scoring_result"."id"
+                           where "report"."create_date" <= $3::date
+                           group by "report"."allocator", "report"."create_date"
+                           order by "report"."allocator", "report"."create_date" desc)
+--
+select "allocator"
+from "allocator_score"
+where "row_index" = 1
+  and "score_percentage" between $1::decimal and $2::decimal;
--- prisma/sql/getAllocatorsReportChecksCountForDay.sql
+++ prisma/sql/getAllocatorsReportChecksCountForDay.sql
@@ -1,15 +1,15 @@
 -- @param {DateTime} $1:day Date for which report checks will be counted
 -- @param {Boolean} $2:check_result? Check result, optional, leave empty to count both failed and passed checks
- WITH check_per_allocator AS
-  (SELECT DISTINCT ON ("report"."allocator") COUNT("check"."id") as "checks_count"
-   FROM "allocator_report" as "report"
-   JOIN "allocator_report_check_result" AS "check" ON "check"."allocator_report_id" = "report"."id"
-   WHERE "check"."create_date" BETWEEN SYMMETRIC date_trunc('day', $1::DATE) AND date_trunc('day', $1::DATE) + '1 day'::INTERVAL
-     AND ($2::BOOLEAN IS NULL
-          OR "check"."result" = $2::BOOLEAN)
-   GROUP BY "report"."allocator",
-            "report"."create_date"
-   ORDER BY "report"."allocator",
-            "report"."create_date" DESC)
-SELECT SUM("checks_count")::INT as "total_checks_count"
-FROM "check_per_allocator"
+
+with "check_per_allocator" as
+         (select distinct on ("report"."allocator") count("check"."id") as "checks_count"
+          from "allocator_report" as "report"
+                   join "allocator_report_check_result" as "check"
+                        on "check"."allocator_report_id" = "report"."id"
+          where "check"."create_date" between symmetric date_trunc('day', $1::date) and date_trunc('day', $1::date) + '1 day'::interval
+            and ($2::boolean is null or "check"."result" = $2::boolean)
+          group by "report"."allocator", "report"."create_date"
+          order by "report"."allocator", "report"."create_date" desc)
+--
+select sum("checks_count")::int as "total_checks_count"
+from "check_per_allocator";
