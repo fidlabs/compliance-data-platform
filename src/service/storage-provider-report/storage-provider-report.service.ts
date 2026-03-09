82a34c775a0bda6e8f8ad8df99afb836d72f6468
lukas <lukasz.wal@neti-soft.com>
2026-03-09 15:55:29 +0100

    Add metrics about SP for client and allocator reports

--- src/service/storage-provider-report/storage-provider-report.service.ts
+++ src/service/storage-provider-report/storage-provider-report.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'src/db/prisma.service';
-import { LotusApiService } from '../lotus-api/lotus-api.service';
 import { IpniMisreportingCheckerService } from '../ipni-misreporting-checker/ipni-misreporting-checker.service';
 import { LocationService } from '../location/location.service';
+import { LotusApiService } from '../lotus-api/lotus-api.service';
+import { StorageProviderUrlFinderService } from '../storage-provider-url-finder/storage-provider-url-finder.service';
 
 @Injectable()
 export class StorageProviderReportService {
@@ -13,6 +14,7 @@ export class StorageProviderReportService {
     private readonly prismaService: PrismaService,
     private readonly lotusApiService: LotusApiService,
     private readonly ipniMisreportingCheckerService: IpniMisreportingCheckerService,
+    private readonly storageProviderUrlFinderService: StorageProviderUrlFinderService,
   ) {}
 
   public async getStorageProviderDistribution(clientId: string) {
@@ -42,15 +44,25 @@ export class StorageProviderReportService {
           minerInfo.result.Multiaddrs,
         );
 
-        const retrievability_success_rate_url_finder =
-          await this.getStorageProviderUrlFinderRetrievability(
+        const {
+          bandwidthMetric,
+          consistentRetrievabilityMetric,
+          inconsistentRetrievabilityMetric,
+          retrievabilitySuccessRateMetric,
+          ttfbMetric,
+        } =
+          await this.storageProviderUrlFinderService.getUrlFinderLastMetricsValuesForProvider(
             clientProviderDistribution.provider,
           );
 
         return {
           ...clientProviderDistribution,
           retrievability_success_rate_url_finder:
-            retrievability_success_rate_url_finder,
+            retrievabilitySuccessRateMetric,
+          consistent_retrievability: consistentRetrievabilityMetric,
+          inconsistent_retrievability: inconsistentRetrievabilityMetric,
+          ttfb: ttfbMetric,
+          bandwidth: bandwidthMetric,
           ipni_reporting_status: ipniReportingStatus.status,
           ipni_reported_claims_count:
             ipniReportingStatus.ipniReportedClaimsCount,
@@ -89,54 +101,5 @@ export class StorageProviderReportService {
       );
 
     return data?.success_rate ?? null;
-  }
-
-  // returns 0 - 1 success_rate, success_rate_http
-  private async getStorageProviderRetrievability(
-    storageProviderId: string,
-  ): Promise<{
-    retrievability_success_rate: number;
-    retrievability_success_rate_http: number;
-  } | null> {
-    const result =
-      // get data from the last 7 full days
-      await this.prismaService.provider_retrievability_daily.aggregate({
-        _sum: {
-          total: true,
-          successful: true,
-          successful_http: true,
-        },
-        where: {
-          provider: storageProviderId,
-          date: {
-            gte: new Date( // a week ago at 00:00
-              new Date(new Date().setDate(new Date().getDate() - 7)).setHours(
-                0,
-                0,
-                0,
-                0,
-              ),
-            ),
-          },
-        },
-      });
-
-    if (result._sum.total > 0) {
-      const retrievability_success_rate =
-        result._sum.successful / result._sum.total;
-
-      const retrievability_success_rate_http =
-        result._sum.successful_http / result._sum.total;
-
-      return {
-        retrievability_success_rate: retrievability_success_rate,
-        retrievability_success_rate_http: retrievability_success_rate_http,
-      };
-    } else {
-      return {
-        retrievability_success_rate: null,
-        retrievability_success_rate_http: null,
-      };
-    }
   }
 }
