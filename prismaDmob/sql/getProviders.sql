24d93c531651318c6715205f034b2744411f21f6
Maciej Trojan <maciej.trojan@neti-soft.com>
2026-07-20 13:55:28 +0200

    fix: provider runner null provider id handling
    - Filter null provider ids in query
    - Refactor provider data handling to use transaction for deletion and insertion

--- prismaDmob/sql/getProviders.sql
+++ prismaDmob/sql/getProviders.sql
@@ -1,18 +1,25 @@
-with "miner_info" as (select "unified_verified_deal"."providerId"                  as "provider",
-                             count(*)                                              as "num_of_deals",
-                             "unified_verified_deal"."clientId"                    as "client",
-                             coalesce(sum("unified_verified_deal"."pieceSize"), 0) as "total_deal_size",
-                             coalesce(max("unified_verified_deal"."termStart"), 0) as "last_deal_height",
-                             coalesce(min("unified_verified_deal"."termStart"), 0) as "first_deal_height"
-                      from "unified_verified_deal"
-                      where "unified_verified_deal"."sectorId" <> '0'
-                      group by "unified_verified_deal"."clientId", "unified_verified_deal"."providerId")
-select 'f0' || "unique_providers"."providerId"                  as "id",
-       coalesce(sum("miner_info"."num_of_deals"), 0)::int       as "num_of_deals",
-       coalesce(sum("miner_info"."total_deal_size"), 0)::bigint as "total_deal_size",
-       count("miner_info"."client")::int                        as "num_of_clients",
-       coalesce(max("miner_info"."last_deal_height"), 0)::int   as "last_deal_height",
-       coalesce(min("miner_info"."first_deal_height"), 0)::int  as "first_deal_height"
-from "unique_providers"
-      left join "miner_info" on "unique_providers"."providerId" = "miner_info"."provider"
-group by "unique_providers"."providerId";
+WITH miner_info AS (
+	SELECT
+		"providerId" AS "provider",
+		COUNT(*) AS num_of_deals,
+		"clientId" AS client,
+		COALESCE(SUM("pieceSize"), 0) AS total_deal_size,
+		COALESCE(MAX("termStart"), 0) AS last_deal_height,
+		COALESCE(MIN("termStart"), 0) AS first_deal_height
+	FROM unified_verified_deal
+	WHERE "sectorId" <> '0'
+	GROUP BY "clientId", "providerId"
+)
+
+SELECT
+	'f0' || up."providerId" AS "id",
+	COALESCE(SUM(mi.num_of_deals), 0)::INT AS num_of_deals,
+	COALESCE(SUM(mi.total_deal_size), 0)::BIGINT AS total_deal_size,
+	COUNT(mi.client)::INT AS num_of_clients,
+	COALESCE(MAX(mi.last_deal_height), 0)::INT AS last_deal_height,
+	COALESCE(MIN(mi.first_deal_height), 0)::INT AS first_deal_height
+FROM unique_providers up
+LEFT JOIN miner_info mi
+	ON up."providerId" = mi.provider
+WHERE up."providerId" IS NOT NULL
+GROUP BY up."providerId";
